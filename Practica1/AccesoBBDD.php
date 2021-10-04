92def2ac35d0b80afa17bdad011781da93acdfd1
juanenn <91121609+juanenn@users.noreply.github.com>
2021-10-04 09:54:54 +0200

    create access bbdd

---
+++ Practica1/AccesoBBDD.php
@@ -0,0 +1,33 @@
+<pre>
+<?php
+    
+    $db_host = "localhost";
+    $db_name = "bd_taller";
+    $db_user = "";
+    $db_pass = "";
+    
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+        exit;
+    }
+    
+    echo "Connected successfully.";
+ 
+    $sql = "SELECT *
+            FROM user
+            ORDER BY date_entry;";
+ 
+    $results = mysqli_query($conn, $sql);
+
+    if ($results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $users = mysqli_fetch_all($results, MYSQLI_ASSOC);
+        print_r($users);
+    }
+ 
+?>
+</pre>
+
