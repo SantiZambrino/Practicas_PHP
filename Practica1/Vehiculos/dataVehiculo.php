fa87b5e7b716ba2ff49dd878eb64eae28f86d26e
JuanfranMonteroMedac <jmt0003@alu.medac.es>
2021-10-06 12:17:11 +0200

    update files and styles

--- Practica1/Vehiculos/dataVehiculo.php
+++ Practica1/Vehiculos/dataVehiculo.php
@@ -1,3 +1,41 @@
+<pre>
 <?php
  
-?>
+     $name = $_GET['nombre'];
+     $apellido = $_GET['apellido'];
+     $db_host = "localhost";
+     $db_name = "bd_taller";
+     $db_user = "root";
+     $db_pass = "2DAW2021...";
+
+     $name = strtolower($name);
+     $apellido = strtolower($apellido);
+     
+     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+     
+     if (mysqli_connect_error()) {
+ 
+         echo 'FALLO EN LA CONEXIÓN';
+         echo mysqli_connect_error();
+         exit;
+     }
+      
+     $sql = "SELECT nombre, apellidos
+             FROM lista_usuario
+             WHERE nombre = "+$name+" AND apellidos = "+$apellido;
+  
+     $results = mysqli_query($conn, $sql);
+ 
+     if ($results === false) {
+         echo mysqli_error($conn);
+     } else {
+         $users = mysqli_fetch_all($results, MYSQLI_ASSOC);
+         print_r($users);
+     }
+ 
+ 
+     mysqli_close($conn);
+  
+ ?>
+ </pre>
+ 
