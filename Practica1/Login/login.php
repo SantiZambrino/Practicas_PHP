9fedf68005a8b534f5084e8a1ff206484d55d9a0
JuanfranMonteroMedac <jmt0003@alu.medac.es>
2021-10-08 10:38:40 +0200

    work

--- Practica1/Login/login.php
+++ Practica1/Login/login.php
@@ -9,7 +9,29 @@
 </head>
 <body>
    
-  <div class="outer-container"></div>
+  <div class="outer-container">
+
+    <div class="container">
+
+      <h3>Talleres Zamoen</h3>
+
+      <form id="form-login" name="form-login" action="#" method="POST">
+
+      <input type="text" id="name_usu" class="input-style" name="name-usu" placeholder="Usuario...">
+      <input type="password" id="pass_usu" class="input-style" name="pass-usu" placeholder="Contraseña...">
+
+      <div class="box-btn">
+      <button  class="btn submit" value="enviar">Enviar</button>
+      <button  class="btn newUser" value="registra">Nuevo Usuario</button>
+      </div>
+
+      <a href="#" id="forgot-passwd">Has olvidado su contraseña?...</a>
+
+      </form>
+
+    </div>
+
+  </div>
 
 </body>
 </html>
