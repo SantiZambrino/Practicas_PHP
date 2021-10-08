55d57f04e30f5ad14ad99a8308cac75338e1a9d7
JuanfranMonteroMedac <jmt0003@alu.medac.es>
2021-10-08 09:34:55 +0200

    modication all folders

--- Practica1/Vehiculos/vehiculos.php
+++ Practica1/Vehiculos/vehiculos.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styleVehiculo.css" />
+    <link rel="stylesheet" href="../styles/styleVehiculo.css" />
     <link rel="stylesheet" href="../header_y_footer.css" />
     <title>Listado Vehiculos | Talleres Zamoen</title>
 </head>
@@ -34,12 +34,7 @@
         ?>
         </h3>
 
-        <table name="datosUsuario" id="datosUsuarios">
-            
-            <th>Matrícula</th>
-            <th>Marca</th>
-            <th>Modelo</th>
-            <th>Año</th>
+        
 
             <?php
  
@@ -62,8 +57,21 @@
                     exit;
                 }
                 
-                $sql = "SELECT matricula, marca, modelo, año
-                        FROM lista_vehiculos";
+                // $sql = "SELECT vh.matricula, vh.marca, vh.modelo, vh.año FROM lista_vehiculos AS vh
+                //     INNER JOIN lista_usuario 
+                //     WHERE vh.id_usuario = lista.usuario.id_usuario"
+                //     AND ;
+
+
+        
+                $sql =  "SELECT matricula, marca, modelo, año 
+                FROM lista_vehiculos
+                WHERE id_usuario = (SELECT id_usuario
+                                    FROM lista_usuario 
+                                    WHERE dni = '$dni')";
+                        
+                        
+    
 
                 $results = mysqli_query($conn, $sql);
             
@@ -71,7 +79,14 @@
                     echo mysqli_error($conn);
                 } else {
 
-                
+                    ?>
+                    <table name="datosUsuario" id="datosUsuarios">
+            
+                        <th>Matrícula</th>
+                        <th>Marca</th>
+                        <th>Modelo</th>
+                        <th>Año</th>
+                    <?php
                     // while($users = mysqli_fetch_array($results)){
                    foreach ($results as $valor) {
                        ?>
@@ -96,7 +111,7 @@
         </table>
     </div>
 
-    <footer></footer>
+    
 
 </body>
 </html>
