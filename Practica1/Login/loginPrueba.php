b598116033ecf9f79a6c02ccbcf51d932e881018
JuanfranMontero <juanfransmr@gmail.com>
2021-10-13 17:53:58 +0200

    new img, view password and add info in "loginPruebas.php

--- Practica1/Login/loginPrueba.php
+++ Practica1/Login/loginPrueba.php
@@ -25,21 +25,78 @@
 
                 <input type="text" id="name_usu" class="input-style" name="name-usu" placeholder="Usuario...">
                 <input type="password" id="pass_usu" class="input-style" name="pass-usu" placeholder="Contraseña...">
+                <img src="../img/invisible-passwd.png" alt="Mostrar Contraseña" id="mostrarContrasena">
           
                 <div class="box-btn">
-                <a href="../Vehiculos/vehiculos.php" class="btn submit">Enviar</a>
+                <a href="../Vehiculos/newvehiculo.php" class="btn submit">Enviar</a>
                 <a  class="btn newUser" href="../Login/newUser.php">Nuevo Usuario</a>
                 </div>
           
             </form>
 
         </div>
-        
-      
-
+    
     </div>
 
-    
+    <script>
+        
+        var mostrarContrasena = document.getElementById("mostrarContrasena");
+        var mostrar = document.getElementById("pass_usu");
+
+        mostrarContrasena.addEventListener('click', function(e){
+
+            e.preventDefault();
+            if (mostrar.type == "password"){
+                mostrar.type = "text";
+                mostrarContrasena.src = '../img/view-passwd.png';
+            }
+            else{
+                mostrar.type = "password";
+                mostrarContrasena.src = '../img/invisible-passwd.png';
+            }
+        });
+
+      </script>
+
+   <?php/*
+        //CREACCIÓN DE SESION PARA COMPARAR SI EL USUARIO EXISTE.
+        //PARA MOSTRAR MENSAJE DE ERROR (https://es.stackoverflow.com/questions/150719/mostrar-mensaje-usuario-y-o-contrase%C3%B1a-incorrecta)
+
+        session_start();
+
+        $nombre = $_POST['name_usu'];
+        $password = $_POST['pass_usu'];
+
+        // se asume conexion en $conn incluido desde conexion.php, ejemlo:
+        $name = $db_host = "localhost";
+                $db_name = "bd_taller";
+                $db_user = "root";
+                $db_pass = "2DAW2021...";
+
+                $name = strtolower($name);
+                
+        $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+        // añadiría un limit 1 a la consulta pues solo esperamos un registro
+        $consulta = mysqli_query ($conn, "SELECT * FROM sesion WHERE user = '$nombre' AND pass = '$password'");  
+
+        // esto válida si la consulta se ejecuto correctamente o no pero en ningún caso válida si devolvió algún registro
+        if(!$consulta){ 
+            // echo "Usuario no existe " . $nombre . " " . $password. " o hubo un error " . 
+            echo mysqli_error($mysqli);
+            // si la consulta falla es bueno evitar que el código se siga ejecutando
+            exit;
+        } 
+        
+        // validemos pues si se obtuvieron resultados 
+        // Obtenemos los resultados con mysqli_fetch_assoc
+        // si no hay resultados devolverá NULL que al convertir a boleano para ser evaluado en el if será FALSE
+        if($user = mysqli_fetch_assoc($consulta)) {
+            // el usuario y la pwd son correctas
+        } else {
+            // Usuario incorrecto o no existe
+        }
+    */?>
 
     
 </body>
