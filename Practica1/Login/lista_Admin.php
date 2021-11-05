502ba1f8b50337c8202b2e0bca5cb987b4c271cc
JuanfranMonteroMedac <jmt0003@alu.medac.es>
2021-11-05 13:25:15 +0100

    change jf 5-11-2021

--- Practica1/Login/lista_Admin.php
+++ Practica1/Login/lista_Admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/styleHeader.css">
+    <link rel="stylesheet" href="../styles/styleHeader.css" />
    
     <link rel="stylesheet" href="../styles/styleLista_Admin.css">
     <link rel="shortcut icon" href="../img/favicon-logo.png" />
@@ -12,44 +12,59 @@
 </head>
 <body>
     <?php
+        session_start();
+        include "../Login/cookie.php";
 
-    include "../biblioteca/funcionesZamoen.php";
+            include "../biblioteca/funcionesZamoen.php";
 
-    $conn = Conexion();
+            $conn = Conexion();
 
-    session_start();
+            $dni = $_SESSION['dni'];
 
-    $dni = $_SESSION['dni'];
+            $sqlName = "SELECT nombre FROM lista_usuario WHERE dni = '$dni'";
 
-    $sqlName = "SELECT nombre FROM lista_usuario WHERE dni = '$dni'";
+            $resultName = mysqli_query($conn, $sqlName);
+                            
+            if ($resultName === false) {
+                    echo mysqli_error($conn);
+            } 
+            else {
 
-    $resultName = mysqli_query($conn, $sqlName);
-                    
-    if ($resultName === false) {
-            echo mysqli_error($conn);
-    } 
-    else {
+                $info = mysqli_fetch_array($resultName);
 
-        $info = mysqli_fetch_array($resultName);
+                $name = $info['nombre'];
 
-        $name = $info['nombre'];
+            }
 
-    }
+            ?>
+        <div class="outer-container">
 
-        ?>
-    <div class="outer-container">
-
-        <header id="cabecera">
+            <header id="cabecera">
             <div class="vacio">
+            
+            </div>  
+            <div class="logo">  
+                <a href="#">
+                    <img id="logo-taller" src="../img/Logo-Coche.png" alt="Logo Talleres Zamoen">
+                </a>
             </div>
-            <a href="#">
-                <img id="logo-taller" src="../img/Logo-Coche.png" alt="Logo Talleres Zamoen">
-            </a>
-            <div class="info-usu">
-                <h3><?php echo ucfirst($name); ?></h3>
-                <a id="btn-logOut" href="../Login/logOut.php">Cerrar Sesión</a>
-            </div>
-        </header>
+            <div class="container-info-header">
+                <div class="info-usu">
+                    <h3><?php echo ucfirst($name); ?></h3>
+                        <?php
+                        if($_SESSION['id_admin'] == 1){
+                            ?>
+                            <a id="btn-Panel" href="../Login/lista_Admin.php">Volver al Panel</a>
+                            <?php
+                        }
+                        ?>
+                        <a id="btn-logOut" href="../Login/logOut.php">Cerrar Sesión</a>
+                </div>
+                <div class="cookies">
+                    <p><?php echo 'Ultima conexion realizada el: '.$_COOKIE[$_SESSION['dni'].'Cookie']; ?></p>
+                </div>
+                </div>
+            </header>
         <?php
 
       
