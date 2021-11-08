e666f7bd056ca259797b2b5ec677c7fc61a454ea
Santi Zambrino <szo0001@alu.medac.es>
2021-11-08 08:39:33 +0100

    funciones Santi

--- Practica1/biblioteca/funcionesZamoen.php
+++ Practica1/biblioteca/funcionesZamoen.php
@@ -1,10 +1,10 @@
 <?php
     function Conexion(){
 
-        $db_host = "localhost";
+        $db_host = "10.192.240.25:3307";
         $db_name = "bd_taller";
-        $db_user = "root";
-        $db_pass = "2DAW2021...";
+        $db_user = "cuentaSanti";
+        $db_pass = "1234";
 
 
         try{
@@ -37,18 +37,63 @@
     
     }
 
+    function crearCookie($nombreCookie){
+        //Creo el nombre de la cookie
+        $GLOBALS["nombre"] = $nombreCookie;
+        //creo el valor de la cookie
+        $date = new DateTime();
+        //Paso el valor a String
+        $valor = $date->format('Y-m-d H:i:s');
+        // El tiempo de expiración es en 1 dia. PHP traduce la fecha al formato adecuado
+        $expiracion = time() + 84600*15;
+        setcookie($GLOBALS["nombre"], $valor, $expiracion, "/");
+        // echo "Ultima conexion relizada el: ".$_COOKIE[$nombre];
+        // return $valor;
+    }
 
-    function validarDNI($dni){
-        $letra = strtoupper(substr($dni, -1)) ;
-    
-        $numeros = substr($dni, 0, -1);
-        $valido = false;
-        if (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros%23, 1) == $letra && strlen($letra) == 1 && strlen ($numeros) == 8 ){
-          $valido=true;
-        }else{
-          $valido=false;
+    function crearServicio($tipoServicio, $descripcionServicio,$conn, $dni, $id_matricula){
+        $sql =  "INSERT INTO lista_servicios (tipo_servicio, descripcion, id_matricula) VALUES ('" . $tipoServicio . "','" . $descripcionServicio . "',$id_matricula)";
+
+        $results = mysqli_query($conn, $sql);
+
+        if ($results == false) {
+
+            echo mysqli_error($conn);
+        } else {
+
+            header('Location: ../Vehiculos/vehiculo.php?dni=' . $dni);
         }
+    }
+
+    function camposCompletados($matricula, $tipoServicio, $descripcionServicio){
+        if (!empty($matricula) && !empty($tipoServicio) && !empty($descripcionServicio)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function crearUsuario($nombre, $apellidos, $dni, $telefono, $email, $contra, $conn){
+        $sql =  "INSERT INTO lista_usuario (nombre, apellidos, dni, telefono, email, contrasena) VALUES ('$nombre', '$apellidos', '$dni', '$telefono', '$email', '$contra')";
+                
+        $results = mysqli_query($conn, $sql);
     
-        return $valido;
-      }
+        if ($results === false) {
+        echo mysqli_error($conn);
+        }
+        else{
+
+            header('Location: ../Vehiculos/vehiculo.php?dni='.$dni );
+        }
+    }
+
+    function noExisteUsuario($nombre, $apellidos, $dni ,$telefono, $email, $contra){
+        if (!empty($nombre) && !empty($apellidos) && !empty($dni) && !empty($telefono) && !empty($email) &&  !empty($contra)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 ?>
